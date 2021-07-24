10e4c8ffba1a6de8fa9dba7a0db7f6041cc640ba
Levi Abellar <85912450+leviabellar@users.noreply.github.com>
2021-07-24 21:51:50 +0800

    Wrap up the getcontacts

---
+++ src/api/contacts.controller.js
@@ -0,0 +1,13 @@
+import ContactsDAO from '../dao/contactsDAO'
+
+export default class ContactsController {
+    static async apiGetContacts(req, res, next) {
+        const { contactsList, totalNumContacts } = await ContactsDAO.getContacts()
+        let response = {
+            contacts: contactsList,
+            filters: {},
+            total_results: totalNumContacts,
+        }
+        res.json(response)
+    }
+}
---
+++ src/dao/contactsDAO.js
@@ -0,0 +1,56 @@
+import { ObjectId } from "bson"
+
+let contacts
+let pbook
+const DEFAULT_SORT = [["first_name", 1]]
+
+export default class ContactsDAO {
+    static async injectDB(conn) {
+        if (contacts) {
+            return
+        }
+        try {
+            pbook = await conn.db('task003')
+            contacts = await conn.db('task003').collection('contacts')
+        } catch (e) {
+            console.error(
+                `Unable to establish a collection handle in contactsDAO: ${e}`
+            )
+        }
+    }
+
+    static async getContacts({
+        filters = null,
+        skip = 0,
+        limit = null,
+    } = {}) {
+        let queryParams = {}
+        if (filters) {
+            //to be added soon
+        }
+        let { query = {}, project = {}, sort = DEFAULT_SORT } = queryParams
+        let cursor
+        try {
+            cursor = await contacts
+              .find(query)
+              .project(project)
+              .sort(sort)
+        } catch (e) {
+            console.error(`Unable to issue find command, ${e}`)
+            return { contactList: [], totalNumContacts: 0 }
+        }
+
+        const displayCursor = cursor.limit(limit).skip(skip)
+
+        try {
+            const contactsList = await displayCursor.toArray()
+            const totalNumContacts = await contacts.countDocuments(query)
+
+            return { contactsList, totalNumContacts }
+        } catch (e) {
+            console.error(
+                `Unable to convert cursor to array or problem counting documents, ${e}`
+            )
+        }
+    }
+}
