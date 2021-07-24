2edcb93ffd0f59571b38e3a775011f721a76fdd1
Levi Abellar <85912450+leviabellar@users.noreply.github.com>
2021-07-24 21:30:15 +0800

    Setup getting contacts in server.js

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import cors from 'cors'
+import contacts from './api/contacts.route.js'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/contacts', contacts)
+app.use('*', (req, res) => res.status(404).json({ error: "not found" }))
+
+export default app
