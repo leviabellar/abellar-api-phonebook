f4d7e272a187dfda0dc409a127210cd21b4926c0
Levi Abellar <85912450+leviabellar@users.noreply.github.com>
2021-07-25 20:36:14 +0800

    Add secured route

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import contacts from './api/contacts.route.js'
 import users from './api/users.route.js'
+import passport from './api/auth/auth.js'
 
 const app = express()
 
@@ -10,6 +11,7 @@ app.use(express.json())
 
 app.use('/contacts', contacts)
 app.use('/users', users)
+app.use('/users/contacts', passport.authenticate('jwt', { session: false }), contacts)
 app.use('*', (req, res) => res.status(404).json({ error: "not found" }))
 
 export default app
